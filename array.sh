ca5adcc6024ff31cd581a740bb56d0c986d98b27
suresh-offcl <sureshgopikhanishk@gmail.com>
2025-02-12 15:59:51 +0530

    knowing about arrays

--- array.sh
+++ array.sh
@@ -2,9 +2,9 @@
 
 fruits=("apple","kiwi","banana")
 
-echo khani have $fruit[0]
-echo khani have $fruit[1]
-echo khani have $fruit[2]
+echo "khani have ${fruit[0]}"
+echo "khani have ${fruit[1]}"
+echo "khani have ${fruit[2]}"
 
-echo khani have $fruit[@]
+echo "khani have ${fruit[@]}"
 
