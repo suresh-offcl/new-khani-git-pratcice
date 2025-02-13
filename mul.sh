fc4c30584102024558bc5f5724a6ddfb5e67c1cd
suresh-offcl <sureshgopikhanishk@gmail.com>
2025-02-13 11:12:28 +0530

    multiplying of two number

--- mul.sh
+++ mul.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-number1=$mul1
-number2=$mul2
+#number1=$mul1
+#number2=$mul2
 
-mul=$(($number1 * $number2))
+#mul=$(($number1 * $number2))
 
-echo "multiplication of both number =${mul}"
+#echo "multiplication of both number =${mul}"
+
+num1=10
+num2=20
+
+ans=$((num1 * num2))
+
+echo $ans
