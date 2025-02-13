83c5f3bc71e14f4b1b47866bfb5f1d0a1904c830
suresh-offcl <sureshgopikhanishk@gmail.com>
2025-02-13 15:44:25 +0530

    installing packages

--- installpackagescondition.sh
+++ installpackagescondition.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
-username=$name
 userid=$( id -u )
 
 if [ $userid -ne 0 ]
 then
-    echo "$username dont have root access,pls get access to do anything"
+    echo " dont have root access,pls get access to do anything"
     exit 1
 else
     dnf list installed mysql
     if [ $? -eq 0 ]
     then
-        echo " $username ,mysql is already installed"
+        echo " mysql is already installed"
         exit 1
     else
         dnf install mysql -y
         dnf list installed mysql
         if [ $? -eq 0 ]
         then
-            echo "$username , mysql is succesfully installed"
+            echo " mysql is succesfully installed"
             exit 1
         else
-            echo "$username , mysql is not installed , facing problem to install"
+            echo " mysql is not installed , facing problem to install"
             exit 1
         fi
     fi
