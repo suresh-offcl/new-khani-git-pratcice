6f50016c343a822617fe2f6eb7f3cd0a8dfd501e
suresh-offcl <sureshgopikhanishk@gmail.com>
2025-02-17 21:15:08 +0530

    loading loops for installing packages

--- loop.sh
+++ loop.sh
@@ -1,59 +1,63 @@
 #!/bin/bash
-Log_folder="/var/log/shell-script"
-script_name=$(echo $0 | cut -d "." -f1)
-time_stamp=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE= "$Log_folder/$script_name-$time_stamp.log"
-mkdir -p $Log_folder
+
+log_file="/var/log/shell-practice-script"
+script_name=$( echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+log_folder="$log_file/$script_name-$time_stamp.log"
+mkdir -p $log_folder
 
 userid=$( id -u )
 
-g="e/[31m"
-r="e/[32m"
-n="e/[0m"
-y="e/[33m"
+G="e/[32m"
+R="e/[31m"
+Y="e/[33m"
+N="e/[0m"
 
 check_root () {
 
-    if [ $userid -ne 0 ]
+    if [ $? -ne 0 ]
     then
-        echo " u dont have root access " |tee -a $LOG_FILE
-    
+        echo "ur not having root access" | tee -a $log_folder
+        exit
     fi 
 }
 
-usage () {
 
-    if [ $# -eq 0 ]
-    then
-        echo -e " kindly add packages with the file name " |tee -a $LOG_FILE
-    fi
+
+usage () {
+    echo " u dont have parameters, pls add packages names ,u want to install it " | tee -a $log_folder
+    exit 1
+   
 }
 
 check_root
-
 if [ $# -eq 0 ]
 then 
-    echo -e " kindly pass the arguments for packages "
+    usage
+fi 
 
 validate () {
 
-    if [ $? -ne 0 ]
+    if [ $? -ne 0]
     then
-        echo -e "$r $package is failed $n" |tee -a $LOG_FILE
+        echo -e "$R $package is not installed, ur facing issues while installing $package $N"
     else
-        echo -e "$g $package is success $n" |tee -a $LOG_FILE
-    fi 
+    echo "package is already installed"
 }
 
+
 for package in $@
-do 
+do
     dnf list installed $package 
     if [ $? -ne 0 ]
-    then
-        echo -e "$y $package is not installed, going to install it $n  " |tee -a $LOG_FILE
-        dnf install $package -y |tee -a $LOG_FILE
+    then 
+        echo -e "$G $package is not installed , going to install $package $N"
+        dnf install $package -y
         validate $? "installing $package"
+        
     else
-        echo -e "$g $package is already installed $n " |tee -a $LOG_FILE
+    echo -e "$G $package is already installed .nothing to do $N"
+21  
     fi 
-done
+done 
+
