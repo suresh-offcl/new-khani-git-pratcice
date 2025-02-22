072688c49eba18f6b7f74f1ebb6a47977ada01c7
suresh-offcl <sureshgopikhanishk@gmail.com>
2025-02-22 11:51:05 +0530

    adding packages to install

--- disk_usage.sh
+++ disk_usage.sh
@@ -6,8 +6,8 @@ SUM=0
 while IFS= read -r line
 do 
     USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
-    SUM=$(($SUM+$USAGE))
+    SUM=$SUM $USAGE
+    echo "$SUM $USAGE "
     
 done <<< $DISK_USAGE
 
-echo "$SUM"
