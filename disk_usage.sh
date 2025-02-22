b98df03602d8e534acb0dbf533b3cea5979ec925
suresh-offcl <sureshgopikhanishk@gmail.com>
2025-02-22 11:20:19 +0530

    adding packages to install

--- disk_usage.sh
+++ disk_usage.sh
@@ -6,5 +6,9 @@ DISK_THRESHOLD=5
 while IFS= read -r line
 do 
     USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
-    echo "$USAGE"
+    PARTITION=$(echo $line |grep xfs |awk -F " " '{print $NF}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        echo "$PARTITION thresold is greater than 5 :: current value : $USAGE. please check"
+    fi 
 done <<< $DISK_USAGE
