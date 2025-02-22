dffb0e16a82df27a59d032d22d939b05c2dfba77
suresh-offcl <sureshgopikhanishk@gmail.com>
2025-02-22 18:29:57 +0530

    adding packages to install

--- disk_usage.sh
+++ disk_usage.sh
@@ -6,7 +6,9 @@ SUM=0
 while IFS= read -r line
 do 
     USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
-    PARITION=$(echo $line | grep xfs | awk -F " " '{PRINT $NF}')
+    #PARITION=$(echo $line | grep xfs | awk -F " " '{PRINT $NF}')
+    PARTITION=$(echo $line | grep xfs | awk -F " " '{print $NF}')
+
     echo "$PARITION"
     
 done <<< $DISK_USAGE
